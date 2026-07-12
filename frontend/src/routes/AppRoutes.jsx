928b611f1307e6020a4332355d5b79ddf3f75c1f
rk495_ <8169005058kgn@gmail.com>
2026-07-12 17:00:14 +0530

    Added Reports and Analytics page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,13 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import Analytics from "../pages/Analytics";
 
 function AppRoutes(){
+  const isAuthenticated = !!localStorage.getItem("token");
+
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/analytics" element={isAuthenticated ? <Analytics /> : <Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/analytics" />} />
       </Routes>
     </BrowserRouter>
   )
